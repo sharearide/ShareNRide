dac4e163415493fe6aee141a9a15b777992c472e
Arun Agarwal <arun2621993@gmail.com>
2015-11-05 22:57:03 +0530

    perfectly working app,date 5/11/15

---
+++ app/src/main/java/com/example/shikhajain/shareride/Interface/Communicator.java
@@ -0,0 +1,12 @@
+package com.example.shikhajain.shareride.Interface;
+
+import android.os.Bundle;
+
+/**
+ * Created by bunty on 11/5/2015.
+ */
+public interface Communicator {
+
+    public void callBookARide(Bundle each_user);
+    public void callRideStatus();
+}
